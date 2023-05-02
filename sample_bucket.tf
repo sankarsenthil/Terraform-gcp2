b9725f53cb3d22aaf0d2db85a7eb9e360a1d3d08
Senthil <Sankarsenthil@gmail.com>
2023-05-01 21:59:18 -0400

    TEST

--- sample_bucket.tf
+++ sample_bucket.tf
@@ -2,5 +2,6 @@ resource "google_storage_bucket" "sample_bucket" {
  name          = "test-cicd-regional"
  project       = "cicd-project-381723"
  location      = "US"
+ uniform_bucket_level_access = true
  force_destroy = true
 }
